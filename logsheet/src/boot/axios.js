2ff66156d30e4cc7bdb871a5187105638148ebcf
Paresh Naik <pareshnaik678@gmail.com>
2023-01-03 18:46:49 +0530

    first commit

---
+++ logsheet/src/boot/axios.js
@@ -0,0 +1,17 @@
+import Vue from "vue";
+import axios from "axios";
+
+Vue.prototype.$axios = axios;
+// ^ ^ ^ this will allow you to use this.$axios
+//       so you won't necessarily have to import axios in each vue file
+
+// const api = axios.create({ baseURL: process.env.API_URL });
+
+//  const api = axios.create({ baseURL: "http://143.110.191.192:4000/" });
+const api = axios.create({ baseURL: "http://localhost:3000/" });
+
+Vue.prototype.$api = api;
+// ^ ^ ^ this will allow you to use this.$api
+//       so you can easily perform requests against your app's API
+
+export { axios, api };
---
+++ logsheet/src/layouts/MainLayout.vue
@@ -0,0 +1,280 @@
+<template>
+  <div class="template">
+    <div class="q-pa-md" style="max-width: 400px">
+      <q-form @submit="onSubmit" @reset="onReset" class="q-pa-md form">
+        <div class="head">
+          <h6 id="headline">STERILISATION STATION LOG SHEET</h6>
+        </div>
+        <div class="date">
+          <label for="date">DATE:</label>
+          <input type="text" id="date" name="date" /><br />
+          <label for="shift">Shift:</label>
+          <input type="text" id="shift" name="shift" /><br />
+        </div>
+        <label for="batch no">BATCH NO:</label>
+        <input type="text" id="batch" name="batch" /><br />
+        <label for="batch no">STERILIZER NO:</label>
+        <input type="text" id="sterilizer" name="sterilizer" /><br />
+        <!-- 1st time peak -->
+        <label for="peak">1st Peak</label>
+        <label for="date" id="time-in">Time In</label>
+        <label for="date" id="time-out">Time Out</label>
+        <div class="q-pa-md">
+          <div class="q-gutter-sm row">
+            <q-input filled v-model="time" mask="time" :rules="['time']">
+              <template v-slot:append>
+                <q-icon name="access_time" class="cursor-pointer">
+                  <q-popup-proxy
+                    cover
+                    transition-show="scale"
+                    transition-hide="scale"
+                  >
+                    <q-time v-model="time">
+                      <div class="row items-center justify-end">
+                        <q-btn
+                          v-close-popup
+                          label="Close"
+                          color="primary"
+                          flat
+                        />
+                      </div>
+                    </q-time>
+                  </q-popup-proxy>
+                </q-icon>
+              </template>
+            </q-input>
+
+            <q-input filled v-model="time" mask="time" :rules="['time']">
+              <template v-slot:append>
+                <q-icon name="access_time" class="cursor-pointer">
+                  <q-popup-proxy
+                    cover
+                    transition-show="scale"
+                    transition-hide="scale"
+                  >
+                    <q-time v-model="time">
+                      <div class="row items-center justify-end">
+                        <q-btn
+                          v-close-popup
+                          label="Close"
+                          color="primary"
+                          flat
+                        />
+                      </div>
+                    </q-time>
+                  </q-popup-proxy>
+                </q-icon>
+              </template>
+            </q-input>
+          </div>
+        </div>
+        <!-- <input type="text" id="peak-1" name="peak-1" /><br /> -->
+        <!-- 2nd time peak -->
+        <label for="peak">2nd Peak</label>
+        <label for="date" id="time-in">Time In</label>
+        <label for="date" id="time-out">Time Out</label>
+        <div class="q-pa-md">
+          <div class="q-gutter-sm row">
+            <q-input filled v-model="time" mask="time" :rules="['time']">
+              <template v-slot:append>
+                <q-icon name="access_time" class="cursor-pointer">
+                  <q-popup-proxy
+                    cover
+                    transition-show="scale"
+                    transition-hide="scale"
+                  >
+                    <q-time v-model="time">
+                      <div class="row items-center justify-end">
+                        <q-btn
+                          v-close-popup
+                          label="Close"
+                          color="primary"
+                          flat
+                        />
+                      </div>
+                    </q-time>
+                  </q-popup-proxy>
+                </q-icon>
+              </template>
+            </q-input>
+
+            <q-input filled v-model="time" mask="time" :rules="['time']">
+              <template v-slot:append>
+                <q-icon name="access_time" class="cursor-pointer">
+                  <q-popup-proxy
+                    cover
+                    transition-show="scale"
+                    transition-hide="scale"
+                  >
+                    <q-time v-model="time">
+                      <div class="row items-center justify-end">
+                        <q-btn
+                          v-close-popup
+                          label="Close"
+                          color="primary"
+                          flat
+                        />
+                      </div>
+                    </q-time>
+                  </q-popup-proxy>
+                </q-icon>
+              </template>
+            </q-input>
+          </div>
+        </div>
+        <!-- 3rd time peak -->
+        <label for="peak">3rd Peak</label>
+        <label for="date" id="time-in">Time In</label>
+        <label for="date" id="psi">40 PSI</label>
+        <label for="date" id="time-out">Time Out</label>
+        <div class="q-pa-md">
+          <div class="q-gutter-sm row">
+            <q-input filled v-model="time" mask="time" :rules="['time']">
+              <template v-slot:append>
+                <q-icon name="access_time" class="cursor-pointer">
+                  <q-popup-proxy
+                    cover
+                    transition-show="scale"
+                    transition-hide="scale"
+                  >
+                    <q-time v-model="time">
+                      <div class="row items-center justify-end">
+                        <q-btn
+                          v-close-popup
+                          label="Close"
+                          color="primary"
+                          flat
+                        />
+                      </div>
+                    </q-time>
+                  </q-popup-proxy>
+                </q-icon>
+              </template>
+            </q-input>
+
+            <q-input filled v-model="time" mask="time" :rules="['time']">
+              <template v-slot:append>
+                <q-icon name="access_time" class="cursor-pointer">
+                  <q-popup-proxy
+                    cover
+                    transition-show="scale"
+                    transition-hide="scale"
+                  >
+                    <q-time v-model="time">
+                      <div class="row items-center justify-end">
+                        <q-btn
+                          v-close-popup
+                          label="Close"
+                          color="primary"
+                          flat
+                        />
+                      </div>
+                    </q-time>
+                  </q-popup-proxy>
+                </q-icon>
+              </template>
+            </q-input>
+            <q-input filled v-model="time" mask="time" :rules="['time']">
+              <template v-slot:append>
+                <q-icon name="access_time" class="cursor-pointer">
+                  <q-popup-proxy
+                    cover
+                    transition-show="scale"
+                    transition-hide="scale"
+                  >
+                    <q-time v-model="time">
+                      <div class="row items-center justify-end">
+                        <q-btn
+                          v-close-popup
+                          label="Close"
+                          color="primary"
+                          flat
+                        />
+                      </div>
+                    </q-time>
+                  </q-popup-proxy>
+                </q-icon>
+              </template>
+            </q-input>
+          </div>
+        </div>
+
+        <label for="cages">No.of.Cages</label>
+        <input type="text" id="cages" name="cages" /><br />
+
+        <label for="remarks">Remarks</label>
+        <input type="text" id="remarks" name="remarks" /><br />
+        <!-- <q-input outlined v-model="text" stack-label :dense="dense" /> -->
+        <div class="button">
+          <q-btn label="Save" type="submit" color="primary" id="save" />
+          <q-btn label="Reset" type="reset" color="primary" id="reset" />
+        </div>
+      </q-form>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+
+const linksList = [
+  {
+    title: "Docs",
+    caption: "quasar.dev",
+    icon: "school",
+    link: "https://quasar.dev",
+  },
+  {
+    title: "Github",
+    caption: "github.com/quasarframework",
+    icon: "code",
+    link: "https://github.com/quasarframework",
+  },
+  {
+    title: "Discord Chat Channel",
+    caption: "chat.quasar.dev",
+    icon: "chat",
+    link: "https://chat.quasar.dev",
+  },
+  {
+    title: "Forum",
+    caption: "forum.quasar.dev",
+    icon: "record_voice_over",
+    link: "https://forum.quasar.dev",
+  },
+  {
+    title: "Twitter",
+    caption: "@quasarframework",
+    icon: "rss_feed",
+    link: "https://twitter.quasar.dev",
+  },
+  {
+    title: "Facebook",
+    caption: "@QuasarFramework",
+    icon: "public",
+    link: "https://facebook.quasar.dev",
+  },
+  {
+    title: "Quasar Awesome",
+    caption: "Community Quasar projects",
+    icon: "favorite",
+    link: "https://awesome.quasar.dev",
+  },
+];
+
+export default defineComponent({
+  name: "MainLayout",
+
+  setup() {
+    const leftDrawerOpen = ref(false);
+
+    return {
+      essentialLinks: linksList,
+      leftDrawerOpen,
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
+      },
+    };
+  },
+});
+</script>
